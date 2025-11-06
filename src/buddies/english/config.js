fe3094e22e8992c4753b74c897ca1684dec098b0
CodingTarik <32739295+CodingTarik@users.noreply.github.com>
2025-11-06 15:27:16 +0200

    fix english

--- src/buddies/english/config.js
+++ src/buddies/english/config.js
@@ -34,12 +34,6 @@ export const englishBuddyConfig = {
   
   customTabs: [
     {
-      id: 'grammar',
-      name: { en: 'Grammar', de: 'Grammatik' },
-      icon: GraduationCap,
-      component: 'GrammarLevels' // A1, A2, B1, B2, C1, C2
-    },
-    {
       id: 'vocabulary',
       name: { en: 'Vocabulary', de: 'Vokabeln' },
       icon: BookText,
